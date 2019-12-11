df8095874ba627d1357dfdda9a5c432f27557be4
Ori Marron <orim@wix.com>
2019-12-11 18:28:06 +0200

    add use-relaod.tsx

---
+++ src/use-reload.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+
+interface Reload {
+  reload(): void
+}
+
+const ReloadContext = React.createContext<Reload | null>(null)
+
+export const Reloadable: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [key, setKey] = React.useState<number>(0)
+  const reload = () => setKey(prevKey => prevKey++)
+  return (
+    <ReloadContext.Provider value={{ reload }}>
+      <React.Fragment key={key}>{children}</React.Fragment>
+    </ReloadContext.Provider>
+  )
+}
+
+export const useRelaod = (): Reload => {
+  const value = React.useContext(ReloadContext)
+  if (value) {
+    return value
+  }
+
+  return {
+    reload: () => {
+      throw Error('to use useRelaod you must be rendered under a <Reloadable> component')
+    }
+  }
+}
